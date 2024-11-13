3ce6c4a13a7880a808ecf3755e6dd16414e1edc2
SAYAN1590 <sayanpaul671@gmail.com>
2024-11-13 23:33:22 +0530

    Updated ClientAppwAPI.spec

--- playwright.config.js
+++ playwright.config.js
@@ -32,8 +32,8 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "retain-on-failure",
-    screenshot:"only-on-failure",
+    // trace: "retain-on-failure",
+    // screenshot:"only-on-failure",
     headless: false
   },
 
--- tests/ClientAppwAPI.spec.js
+++ tests/ClientAppwAPI.spec.js
@@ -1,6 +1,10 @@
 const {test, expect, request}= require('@playwright/test');
 const loginPayLoad = {userEmail:"chhutkuritam@gmail.com",userPassword:"Iamsrk@1590"};
 let token;
+
+const orderPayLoad = {orders: [{country: "Cuba", productOrderedId: "6581ca399fd99c85e8ee7f45"}]};
+let orderID;
+
 test.beforeAll( async()=>
 {
     const apiContext = await request.newContext()
@@ -13,76 +17,31 @@ test.beforeAll( async()=>
     const loginResponseJson = await loginResponse.json();
     token = loginResponseJson.token;
     console.log(token);
+
+    const orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+        {
+            data: orderPayLoad,
+            headers:
+            {
+                'Authorization':token,
+                'Content-Type':'application/json'
+            }
+        })
+    const orderResponseJson = await orderResponse.json();
+    orderID= orderResponseJson.orders[0];
+    console.log(orderID);
+
 });
 
 
 test('Client App Test', async ({page})=>
 {
-    page.addInitScript(value => {
+    await page.addInitScript(value => {
         window.localStorage.setItem('token',value);
-
     },token);
 
-    const userName= page.locator("#userEmail");
-    const password = page.locator("#userPassword");
-    const signIn = page.locator("[value='Login']");
-    const products = page.locator(".card-body");
-    const productName= "ZARA COAT 3";
-    const email= "chhutkuritam@gmail.com";
     await page.goto("https://rahulshettyacademy.com/client");
-    await products.last().locator("b").waitFor(); //alternative way
-    const count = await products.count();
-    //ZARA COAT 3
-    for(let i=0;i< count;++i)
-    {
-        
-        if(await products.nth(i).locator("b").textContent()=== productName)
-        {
-            //add to cart
-            console.log("Match successful");
-            await products.nth(i).locator("text= Add To Cart").click();
-            break;
-        }
-    }
-    // Go to Cart Page
-    await page.locator("[routerlink*='cart']").click();
-    await page.locator("div li").first().waitFor();
-    const bool = await page.locator ("h3:has-text('ZARA COAT 3')").isVisible();
-    expect(bool).toBeTruthy();
-
-    //click on Checkout button
-    await page.locator("text=Checkout").click();
-
-    //Enter the Country Name
-    await page.locator("[placeholder*='Country']").pressSequentially("ind");
-    const countryOptionsDropDown = page.locator(".ta-results");
-    await countryOptionsDropDown.waitFor();
-    const optionsCount= await countryOptionsDropDown.locator("button").count();
-    for( let i =0; i< optionsCount; ++i)
-    {
-        const text= await countryOptionsDropDown.locator("button").nth(i).textContent();
-        if(text.trim() === "India")
-        {
-            await countryOptionsDropDown.locator("button").nth(i).click();
-            break;
-        }
-    }
-
-    // Validate the login email in the Place-Order Page
-    await expect(page.locator(".user__name [type= 'text']").first()).toHaveText(email);
-
-    //click on the place order button
-    await page.locator(".action__submit").click();
-
-    // Validate the successful order placement message
-
-    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-
-    // Copy the order id
-
-    const orderID  = await page.locator(".em-spacer-1 .ng-star-inserted").textContent()
-    console.log(orderID);
-
+    
     //Go to Orders Page
 
     await page.locator(".btn[routerlink*='myorders']").click();
