8bad267518b7fdf05c93981f9b32d5792f0e9abb
SAYAN1590 <sayanpaul671@gmail.com>
2024-11-13 11:46:54 +0530

    Updated headless as true

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,7 @@ module.exports = defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "retain-on-failure",
     screenshot:"only-on-failure",
-    headless: false
+    headless: true
   },
 
   /* Configure projects for major browsers */
